d892ca848c7f2ed64f4d7232f6f5d807c6101e21
danielflorez <daniel.florez@yuxiglobal.com>
2018-12-10 17:01:00 -0500

    Project 2

--- 06/app.js
+++ 06/app.js
@@ -1,3 +1,38 @@
 var budgetController = (function () {
-    console.log('Hello');
+
 })();
+
+
+
+var UIController = (function () {
+
+})();
+
+
+
+var controller = (function (budgetCtrl, UICtrl) {
+
+    var ctrlAddItem = function () {
+        // get input data
+
+        // add the item to the budget controller
+
+        // add new item to the UI
+
+        // calculate the budget
+
+        // display the budget on UI
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+
+
+
+
+})(budgetController, UIController);
